bc1a19ade6fdaa25affe3f5885b129984f65eea2
MichaelRomeroJr <MichaelRomeroJr1@Gmail.com>
2020-11-04 16:41:34 -0800

    Update misc.py

--- kenpompy/misc.py
+++ kenpompy/misc.py
@@ -6,37 +6,165 @@ usable pandas dataframes.
 import mechanicalsoup
 import pandas as pd
 from bs4 import BeautifulSoup
+import pathlib
+
+def pom_rating_name_clean_up(dataframe, year=None):
+	for index in dataframe.index:
+		iter_team = dataframe.loc[index,'Team']
+		iter_conf = dataframe.loc[index,'Conf']
+
+		try:
+			if iter_team[-3:] == "St.":
+				dataframe.loc[index,'Team'] = dataframe.loc[index,'Team'][:-1] + "ate"
+				
+			if iter_team == 'Miami OH':	
+				dataframe.loc[index,'Team'] = 'Miami (OH)'
+			if iter_team == "Miami FL":	
+				dataframe.loc[index,'Team'] = "Miami (FL)"
+			if iter_team == "St. Francis PA":	
+				dataframe.loc[index,'Team'] = "St. Francis (PA)"
+			if iter_team == "Bryant":	
+				dataframe.loc[index,'Team'] = "Bryant University"
+			if iter_team == "Southern":	
+				dataframe.loc[index,'Team'] = "Southern University"
+			if iter_team == "Bethune Cookman":	
+				dataframe.loc[index,'Team'] = "Bethune-Cookman"
+			if iter_team == "Cal Baptist":	
+				dataframe.loc[index,'Team'] = "California Baptist"	
+			if iter_team == "The Citadel":
+				dataframe.loc[index,'Team'] = "Citadel"	
+			if iter_team == "Central Connecticut":
+			 	dataframe.loc[index,'Team'] = "Central Connecticut State"
+			if iter_team == "Loyola MD":
+			 	dataframe.loc[index,'Team'] = "Loyola (MD)"
+			if iter_team == "Cal St. Bakersfield":	
+				dataframe.loc[index,'Team'] = "Cal State Bakersfield"
+			if iter_team == "LIU":	
+				dataframe.loc[index,'Team'] = "LIU Brooklyn"
+			if iter_team == "NJIT":	
+				dataframe.loc[index,'Team'] = "N.J.I.T."
+			if iter_team == "Louisiana Monroe":	
+				dataframe.loc[index,'Team'] = "Louisiana-Monroe"
+			if iter_team == "Illinois Chicago":	
+				dataframe.loc[index,'Team'] = "Illinois-Chicago"
+			if iter_team == "Gardner Webb":	
+				dataframe.loc[index,'Team'] = "Gardner-Webb"
+			if iter_team == "Nicholls St.":	
+				dataframe.loc[index,'Team'] = "Nicholls State"
+			if iter_team == "Cal St. Northridge":	
+				dataframe.loc[index,'Team'] = "Cal State Northridge"
+			if iter_team == "UNC Wilmington":	
+				dataframe.loc[index,'Team'] = "North Carolina-Wilmington"
+			if iter_team == 'Mississippi':	
+				dataframe.loc[index,'Team'] = 'Ole Miss'
+			if iter_team == 'Tennessee Martin':	
+				dataframe.loc[index,'Team'] = 'Tennessee-Martin'
+			if iter_team == 'SIU Edwardsville':	
+				dataframe.loc[index,'Team'] = 'SIU-Edwardsville'
+			if iter_team == 'Montana St.':	
+				dataframe.loc[index,'Team'] = 'Montana State'
+			if iter_team == 'Nebraska Omaha':	
+				dataframe.loc[index,'Team'] = 'Nebraska-Omaha'
+			if iter_team == "Long Beach":
+				dataframe.loc[index,'Team'] = "Long Beach State"	
+
+			# Changes unique dataframe values of kenpom.com/index 
+			if iter_team == "Saint Mary" and iter_conf == "WCC":
+				dataframe.loc[index,'Team'] = "Saint Mary's"
+			if iter_team == "North Carolina Central " and iter_conf == "MEAC":
+				dataframe.loc[index,'Team'] = "North Carolina Central"
+			if iter_team == "Boston University " and iter_conf == "Pat":
+				dataframe.loc[index,'Team'] = "Boston University"
+			if iter_team == "Texas A&M Corpus Chris" and iter_conf == "Slnd":
+				dataframe.loc[index,'Team'] = "Texas A&M-CC"
+			if iter_team == "Maryland Eastern Shore" and iter_conf == "MEAC":
+				dataframe.loc[index,'Team'] = "Maryland-Eastern Shore"
+			if iter_team == "St. Francis NY" and iter_conf == "NEC": 	
+				dataframe.loc[index,'Team'] = "St. Francis (BKN)"
+			if iter_team == "Saint Joseph's" and iter_conf == "A10": 	
+				dataframe.loc[index,'Team'] = "Saint Joseph's (PA)"
+			if iter_team == "Saint Peter's" and iter_conf == "MAAC": 	
+				dataframe.loc[index,'Team'] = "St. Peter's"
+			if iter_team == "Cal St. Fullerton" and iter_conf == "BW": 	
+				dataframe.loc[index,'Team'] = "Cal State Fullerton"
+			if iter_team == "Arkansas Pine Bluff" and iter_conf == "SWAC":
+				dataframe.loc[index,'Team'] = "Arkansas-Pine Bluff"
+			if iter_team == "College of Charleston" and iter_conf == "CAA": # for years < 2019
+				dataframe.loc[index,'Team'] = "Charleston"
+
+		except:
+			pass
+		pass
+	return dataframe
+
+
 
 def get_pomeroy_ratings(browser, season=None):
-    """
-    Scrapes the Pomeroy College Basketball Ratings table (https://kenpom.com/index.php) into a dataframe.
+	"""
+	Scrapes the Pomeroy College Basketball Ratings table (https://kenpom.com/index.php) into a dataframe.
 
-    Args:
-        browser (mechanicalsoup StatefulBrowser): Authenticated browser with full access to kenpom.com generated
-            by the `login` function.
-        season (str, optional): Used to define different seasons. 2002 is the earliest available season.
-            Most recent season is the default.
-    Returns:
-        refs_df (pandas dataframe): Pandas dataframe containing the Pomeroy College Basketball Ratings table from kenpom.com.
-    Raises:
-        ValueError: If `season` is less than 2002.
-    """
-    url = 'https://kenpom.com/index.php'
-    if season and int(season) < 2002:
-        raise ValueError("season cannot be less than 2002")
-    url += '?y={}'.format(season)
-    browser.open(url)
-    page = browser.get_current_page()
-    table = page.find_all('table')[0]
-    ratings_df = pd.read_html(str(table))
-    # Dataframe tidying.
-    ratings_df = ratings_df[0]
-    ratings_df.columns = ratings_df.columns.map(lambda x: x[1])
-    # Parse out seed, most current won't have this
-    tmp = ratings_df['Team'].str.extract('(?P<Team>[a-zA-Z]+\s*[a-zA-Z]+\.*)\s*(?P<Seed>\d*)')
-    ratings_df["Team"] = tmp["Team"]
-    ratings_df["Seed"] = tmp["Seed"]
-    return ratings_df
+	Args:
+		browser (mechanicalsoup StatefulBrowser): Authenticated browser with full access to kenpom.com generated
+			by the `login` function.
+		season (str, optional): Used to define different seasons. 2002 is the earliest available season.
+			Most recent season is the default.
+	Returns:
+		refs_df (pandas dataframe): Pandas dataframe containing the Pomeroy College Basketball Ratings table from kenpom.com.
+	Raises:
+		ValueError: If `season` is less than 2002.
+	"""
+	url = 'https://kenpom.com/index.php'
+	if season and int(season) < 2002:
+		raise ValueError("season cannot be less than 2002")
+	url += '?y={}'.format(season)
+	browser.open(url)
+	page = browser.get_current_page()
+	table = page.find_all('table')[0]
+	ratings_df = pd.read_html(str(table))
+	# Dataframe tidying.
+	ratings_df = ratings_df[0]
+	ratings_df.columns = ratings_df.columns.map(lambda x: x[1])
+
+	for index in ratings_df.index:
+		try:
+			iter_team = ratings_df.loc[index,'Team']
+			#print(iter_team)
+			# format the team name 
+			if iter_team[-1] == '*': # if it ends in '*'
+				iter_team = iter_team.replace('*', '')
+
+				# for character in iter_team: # remove rank 
+				# 	print(character)
+				# 	if character.isdigit():
+				# 		iter_team = iter_team.replace(character, '')
+
+			for character in iter_team: # remove rank 
+				#print(character)
+				if character.isdigit():
+					iter_team = iter_team.replace(character, '')
+			
+			if iter_team[-1] == ' ': # if it ends in ' '
+				iter_team = iter_team[:-1]
+
+			ratings_df.loc[index,'Team'] = iter_team
+		except Exception as e:
+			
+			path_str = pathlib.Path(__file__).parent.absolute()
+			#print('Error: ',  path_str, '\n', e)
+			
+			pass
+	# Parse out seed, most current won't have this
+	tmp = ratings_df['Team'].str.extract('(?P<Team>[a-zA-Z]+\s*[a-zA-Z]+\.*)\s*(?P<Seed>\d*)')
+	#ratings_df["Team"] = tmp["Team"]
+	ratings_df["Seed"] = tmp["Seed"]
+
+	#print(ratings_df)
+	#print()
+	#print(pom_rating_name_clean_up(ratings_df))
+
+	ratings_df = pom_rating_name_clean_up(ratings_df, year=season)
+
+	return ratings_df
 
 
 def get_trends(browser):
